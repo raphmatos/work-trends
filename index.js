f64b8b8fb193b4341f7d74d2fe6bd1dd7703e938
Raphael Matos <raphmatos@gmail.com>
2025-01-27 22:22:56 +0000

    MVR pattern

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const consign = require('consign');
+
+const PORT = 3000;
+const app = express();
+
+app.set('json spaces', 4);
+
+consign()
+.include('models')
+.then('routes')
+.into(app);
+
+app.listen(PORT, () => {
+console.log(`NTask API - porta ${PORT}`);
+});
