fd72f7d44b981fdd13632864d8a198e3ab6932f5
Raphael Matos <raphmatos@gmail.com>
2025-01-28 20:01:48 +0000

    Architecture improved

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const consign = require('consign');
 
-const PORT = 3000;
 const app = express();
 
-app.set('json spaces', 4);
-
 consign()
-.include('models')
-.then('routes')
-.into(app);
-
-app.listen(PORT, () => {
-console.log(`NTask API - porta ${PORT}`);
-});
+    .include('models')
+    .then('middlewares.js')
+    .then('routes')
+    .then('boot.js')
+    .into(app);
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,8 @@
 module.exports = app => {
-    app.get('/tasks', (req, res) => {
-    res.json({
-    tasks: [
-    { title: 'Fazer compras' },
-    { title: 'Consertar o pc' }
-    ]
-    });
+    const Tasks = app.models.tasks;
+        app.get('/tasks', (req, res) => {
+        Tasks.findAll({}, (tasks) => {
+        res.json({ tasks });
+        });
     });
 };
